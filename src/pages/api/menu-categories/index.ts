dc5a6eefc90fdfa09e2112316668a5853032d4df
MSquare FDC <msquarefdc@gmail.com>
2023-11-03 00:30:30 +0700

    episode36

--- src/pages/api/menu-categories/index.ts
+++ src/pages/api/menu-categories/index.ts
@@ -38,41 +38,79 @@ export default async function handler(
     });
     return res.status(200).json({ menuCategory });
   } else if (method === "DELETE") {
-    const menuCategoryId = Number(req.query.id);
-    const menuCategory = await prisma.menuCategory.findFirst({
-      where: { id: menuCategoryId },
-    });
-    if (!menuCategory) return res.status(400).send("Bad request.");
-    /*
-    const menuCategoryMenus = await prisma.menuCategoryMenu.findMany({
-      where: { menuCategoryId },
-    });
-    const menuIds = menuCategoryMenus.map((item) => item.menuId);
-    menuIds.forEach(async (menuId: number) => {
-      const menuCategoryMenus = await prisma.menuCategoryMenu.findMany({
+    const menuCategoryId = Number(req.query.id); // MC1
+    const menuIds = (
+      await prisma.menuCategoryMenu.findMany({
+        where: { menuCategoryId, isArchived: false },
+      })
+    ).map((item) => item.menuId);
+    const menuIdsPromise = menuIds.map(async (menuId) => {
+      const menuData = { menuId, count: 1 };
+      const count = await prisma.menuCategoryMenu.count({
         where: { menuId, isArchived: false },
       });
-      if (menuCategoryMenus.length === 1) {
-        // one menu is connected to only one menu category
-        await prisma.menuCategoryMenu.updateMany({
-          data: { isArchived: true },
-          where: { menuCategoryId, menuId },
+      menuData.count = count;
+      return menuData;
+    });
+    const menuIdsToArchive = (await Promise.all(menuIdsPromise))
+      .filter((item) => item.count === 1)
+      .map((item) => item.menuId);
+    console.log(menuIdsToArchive);
+    const addonCategoryIds = (
+      await prisma.menuAddonCategory.findMany({
+        where: { menuId: { in: menuIdsToArchive }, isArchived: false },
+      })
+    ).map((item) => item.addonCategoryId);
+    const addonCategoryIdsPromise = addonCategoryIds.map(
+      async (addonCategoryId) => {
+        const addonCategoryData = { addonCategoryId, count: 1 };
+        const count = await prisma.menuAddonCategory.count({
+          where: { addonCategoryId, isArchived: false },
         });
-        // menuAddonCategory
-        await prisma.menuAddonCategory.updateMany({
-          data: { isArchived: true },
-          where: {
-            menuId,
-          },
-        });
-      } else {
-        // one menu is connected to many menu cateogories
-        await prisma.menuCategoryMenu.updateMany({
-          data: { isArchived: true },
-          where: { menuCategoryId },
-        });
+        addonCategoryData.count = count;
+        return addonCategoryData;
       }
-    });*/
+    );
+    const addonCategoryIdsToArchive = (
+      await Promise.all(addonCategoryIdsPromise)
+    )
+      .filter((item) => item.count === 1)
+      .map((item) => item.addonCategoryId);
+    console.log(addonCategoryIdsToArchive);
+    /*
+      1. delete menuCategoryMenu
+      2. delete menuAddonCategory
+      3. delete addonCategory
+      4. delete addon
+      5. delete menu
+      6. delete menuCategory
+    */
+    for (const menuId of menuIdsToArchive) {
+      await prisma.menu.updateMany({
+        data: { isArchived: true },
+        where: { id: menuId },
+      });
+      await prisma.menuAddonCategory.updateMany({
+        data: { isArchived: true },
+        where: { menuId },
+      });
+    }
+    for (const addonCategoryId of addonCategoryIdsToArchive) {
+      await prisma.addonCategory.updateMany({
+        data: { isArchived: true },
+        where: { id: addonCategoryId },
+      });
+      await prisma.addon.updateMany({
+        data: { isArchived: true },
+        where: { addonCategoryId },
+      });
+    }
+    for (const menuId of menuIds) {
+      await prisma.menuCategoryMenu.updateMany({
+        data: { isArchived: true },
+        where: { menuId, menuCategoryId },
+      });
+    }
     await prisma.menuCategory.update({
       data: { isArchived: true },
       where: { id: menuCategoryId },
