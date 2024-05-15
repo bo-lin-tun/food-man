395e82b70845c18428e8f8f4370346c947617b8c
San Wan Aung <sanwanaung400042@gmail.com>
2024-05-15 23:04:01 +0630

    fixed socket connection error

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,19 +1,7 @@
-// import { Server as NetServer, Socket } from "net";
-// import { NextApiResponse } from "next";
-// import { Server as SocketIOServer } from "socket.io";
-
-// export type NextApiResponseServerIo = NextApiResponse & {
-//   socket: Socket & {
-//     server: NetServer & {
-//       io: SocketIOServer;
-//     };
-//   };
-// };
-
 import type { Server as HTTPServer } from "http";
 import type { NextApiResponse } from "next";
 import type { Socket as NetSocket } from "net";
-import { Server as IOServer } from "";
+import { Server as IOServer } from "socket.io";
 
 interface SocketServer extends HTTPServer {
   io?: IOServer | undefined;
