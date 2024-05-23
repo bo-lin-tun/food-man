60d0e2683012c5addd1ea1527705cf9291371682
wminaung <minwin243@gmail.com>
2024-05-24 01:56:25 +0630

    wew again

--- next.config.js
+++ next.config.js
@@ -17,6 +17,28 @@ const nextConfig = {
       // },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Set CORS headers to allow requests from any origin
+        source: "/images/foodman", // Adjust the source path as needed
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow requests from any origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS", // Allow specified HTTP methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, X-Requested-With, Content-Type, Accept", // Allow specified headers
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
