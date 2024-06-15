48bc97e02cbb9c57f057df93831c78fa15260415
bo-lin-tun <tngwe6514@gmail.com>
2024-06-15 12:12:18 +0630

    change socket url

--- src/pages/api/socket/index.ts
+++ src/pages/api/socket/index.ts
@@ -17,7 +17,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
       path: "/api/socket",
       addTrailingSlash: false,
       cors: {
-        origin: "http://localhost:3000",
+        origin: "https://food-man.vercel.app",
         methods: ["GET", "POST"],
         credentials: true,
       },
